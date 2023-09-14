bfeba0b89bd71c47f6103ed8dfb06db2474aa2dd
Marco Lioi <marcolioi1996@gmail.com>
2023-09-14 17:34:48 +0200

    letto il file csv

--- query.py
+++ query.py
@@ -2,3 +2,10 @@ from pyspark.sql import *
 from pyspark.sql.functions import *
 
 spark = SparkSession.builder.appName("SABD").getOrCreate()
+
+df = spark.read \
+    .option("header", "true").option("comment", "#") .csv(r"C:\Users\Marco Lioi\Desktop\spark-project\out500_combined+header.csv")
+
+df.printSchema()
+
+spark.stop()
