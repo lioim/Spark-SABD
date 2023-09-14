9c5b14e53ab0385d0b77e9fd433f94605a1900b7
Marco Lioi <marcolioi1996@gmail.com>
2023-09-14 18:30:24 +0200

    calcolato minimo

--- query.py
+++ query.py
@@ -6,6 +6,17 @@ spark = SparkSession.builder.appName("SABD").getOrCreate()
 df = spark.read \
     .option("header", "true").option("comment", "#") .csv(r"C:\Users\Marco Lioi\Desktop\spark-project\out500_combined+header.csv")
 
-df.printSchema()
+df1 = df.select("ID", "SecType", "Date", "Time", col("Close").cast("double"))
+df1 = df1.withColumn("Hour", substring(col("Time"), 1, 2).cast("int"))
+df1.createOrReplaceTempView("table1")
+
+query = """SELECT Date,Hour, MIN(Close) 
+FROM table1 
+WHERE SecType = "E" 
+AND ID LIKE "%FR" 
+GROUP BY Date,Hour
+ORDER BY Hour
+"""
+spark.sql(query).show()
 
 spark.stop()
